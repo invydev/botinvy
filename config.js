b44e928eef65c195b99d56a7e83361ab1a65205d
invydev <priinnnceess71@gmail.com>
2025-11-02 02:00:10 +0700

    Upload file: config.js

---
+++ config.js
@@ -0,0 +1,25 @@
+module.exports = {
+    
+    ownerId: 6674281210, // ID LU
+    
+    telegramBotToken: "8240210852:AAHfDoo47hDipoeLfOh62ZPya0AN1Hj0igw", // TOKEN LU
+
+    sessionName: "session",
+
+
+    photoStart: "https://files.catbox.moe/y72ctj.jpg",
+
+    message: {
+        owner: ` LU SIAPA ANJING?!`,
+        premium: ` MINIMAL GA GRATISAN LAH`,
+        wait: ` Otw boskuu, sabar ya...`,
+        error: ` ERROR KONTOL`,
+        waNotConnected: ` WhatsApp belum terkoneksi! /pair 628xxx`
+    },
+
+    settings: {
+        namabot: "Cek Bio By ZYGIT",
+        footer: "Powered By ZYGIT",
+        cekBioBatchSize: 20 // Jumlah nomor yang dicek bersamaan.
+    }
+};
